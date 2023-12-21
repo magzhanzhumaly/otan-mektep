f36a8faac14b6c0330611157a6ab31814ffa4dba
Magzhan Zhumaly <49714105+magzhanzhumaly@users.noreply.github.com>
2023-12-21 09:59:09 +0600

    Delete Pods directory

--- Pods/Target Support Files/Pods-otan-mektep-otan-mektepUITests/Pods-otan-mektep-otan-mektepUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_otan_mektep_otan_mektepUITests : NSObject
-@end
-@implementation PodsDummy_Pods_otan_mektep_otan_mektepUITests
-@end
--- Pods/Target Support Files/Pods-otan-mektep/Pods-otan-mektep-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_otan_mektep : NSObject
-@end
-@implementation PodsDummy_Pods_otan_mektep
-@end
