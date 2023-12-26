52a381949ef1de30cfcda5ecb0d3cac085d13ffb
Magzhan Zhumaly <magzhanzhumaly@gmail.com>
2023-12-26 11:23:59 +0600

    fix error, populate with vcs

---
+++ otan-mektep/Controllers/Pupil/Dining/PupilDiningCartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PupilDiningCartViewController.swift
+//  otan-mektep
+//
+//  Created by Magzhan Zhumaly on 26.12.2023.
+//
+
+import UIKit
+
+class PupilDiningCartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
